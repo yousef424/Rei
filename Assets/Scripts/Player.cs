ace55b1415127eba6a254532d1ceaba6eafcffeb
yousef424 <yousef424ae@yahoo.com>
2017-07-24 12:23:54 +0400

    This is the beginning

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public GameObject shotbullet;
+    public GameObject Bullet;
+    private Rigidbody bulletbody;
+    private Rigidbody playerbody;
+    public float enginepower;
+    public float ammo = 2;
+	// Use this for initialization
+	void Start () {
+        
+        playerbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerbody.AddForce(transform.forward * enginepower);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerbody.AddForce(-transform.forward * enginepower);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerbody.AddForce(transform.right * enginepower);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerbody.AddForce(-transform.right * enginepower);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && ammo==2 || ammo==1)
+        {
+            shoot();
+        }
+       
+
+    }
+    public void shoot()
+    {
+        shotbullet = Instantiate(Bullet, transform.position, Quaternion.identity);
+        bulletbody=shotbullet.GetComponent<Rigidbody>();
+        bulletbody.AddForce(transform.forward, ForceMode.Impulse);
+    }
+}
+
---
+++ Assets/Scripts/destroy_bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy_bullet : MonoBehaviour {
+    private float destorytime = 0.5f;
+    public GameObject bullet;
+    // Use this for initialization
+    void Start () {
+       
+    }
+
+    // Update is called once per frame
+    void Update() {
+        bullet = GameObject.FindWithTag("bullet");
+        Destroy(bullet, destorytime);
+    }
+        
+}
